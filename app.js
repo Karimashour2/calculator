1e68517e0b714a9243ed7b54ae33ea10dd28b521
karimashour2 <karim.note10.l@gmail.com>
2022-12-22 22:01:38 +0200

    enhance calculator

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
 
+let isEqual = false;
+
+const equalFn= ()=>{
+  screenElement.value===''? screenElement.value='':
+  screenElement.value= Number.isInteger(eval(screenElement.value))?
+  eval(screenElement.value) : eval(screenElement.value).toFixed(2);
+  isEqual = true;
+} 
+document.body.addEventListener('keydown', (e)=> {
+if(e.key==='Enter'){
+  equalFn()
+}else if(!isNaN(e.key)){
+  if(isEqual){
+    screenElement.value = e.key;
+  }else{
+    screenElement.value += e.key;
+  }
+  isEqual=false;
+} else if (['+', '-', '*', '/', '.'].includes(e.key)) {
+  screenElement.value += e.key;
+}
+if(e.key === 'Backspace') {
+  screenElement.value = screenElement.value.substring(0, screenElement.value.length - 1);
+}
+e.preventDefault();
+})
 
 let buttonsElements= document.querySelectorAll('.btn');
 let screenElement= document.querySelector('input');
@@ -9,15 +35,16 @@ screenElement.focus();
 
 buttonsElements.forEach((button)=>{
   button.onclick=(e)=> {
+    if(isEqual){
+      screenElement.value = '';
+      isEqual = false;
+    }
     screenElement.value += e.target.value
   }
 });
 
 clearBtn.onclick= ()=> screenElement.value = ''
 
-let answer= equalBtn.addEventListener('click',(e)=>{
-  screenElement.value===''? screenElement.value='':
-  screenElement.value= Number.isInteger(eval(screenElement.value))?
-  eval(screenElement.value) : eval(screenElement.value).toFixed(2)
-})
+equalBtn.addEventListener('click', equalFn)
+
 
